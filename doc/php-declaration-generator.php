3e6ef9e58d707222dfbba21975b798d66b89c671
WalterWoshid <wotschel.valentin@googlemail.com>
2022-02-12 02:14:15 +0100

    Updated php-declaration-generator.php

--- doc/php-declaration-generator.php
+++ doc/php-declaration-generator.php
@@ -1,34 +1,53 @@
 <?php
+/** @noinspection PhpUnused */
+/** @noinspection PhpUnhandledExceptionInspection */
 
-define('T', '    ');
-define('N', PHP_EOL);
+const T = '    ';
+const N = PHP_EOL;
 
-$functions = array('aop_add_before', 'aop_add_after', 'aop_add_around');
-$classes = array('AopJoinPoint');
+// Generate a PHP declaration for the given type.
+$functions = ['aop_add_around', 'aop_add_before', 'aop_add_after', 'aop_add_after_returning', 'aop_add_after_throwing'];
+$classes = ['AopJoinPoint'];
 $constant_prefix = 'AOP';
 
-$php = '<?php' . N;
-$php .= '/**' . N . ' * Generated stub file for code completion purposes' . N . ' */';
-$php .= N . N;
 
-//echo ReflectionClass::export('ReflectionMethod');
+
+// Beginning
+$php = '<?php' . N . N;
+$php .= '/**' . N . ' * Generated stub file for code completion purposes' . N . ' */';
+$php .= N;
+
+
+
+// Constant definitions
+$constants = [];
 foreach (get_defined_constants() as $cname => $cvalue) {
-    if (strpos($cname, $constant_prefix) === 0) {
-        $php .= 'define(\'' . $cname . '\', ' . $cvalue . ');' . N;
+    if (str_starts_with($cname, $constant_prefix)) {
+        $constants[$cname] = $cvalue;
     }
+}
+
+asort($constants);
+
+foreach ($constants as $cname => $cvalue) {
+    $php .= 'define(\'' . $cname . '\', ' . $cvalue . ');' . N;
 }
 
 $php .= N;
 
+
+
+// Functions
 foreach ($functions as $function) {
+    /** @noinspection PhpUnhandledExceptionInspection */
     $refl = new ReflectionFunction($function);
     $php .= 'function ' . $refl->getName() . '(';
     foreach ($refl->getParameters() as $i => $parameter) {
         if ($i >= 1) {
             $php .= ', ';
         }
-        if ($typehint = $parameter->getClass()) {
-            $php .= $typehint->getName() . ' ';
+        if (($type = $parameter->getType()) === 'class') {
+            $php .= $type->getName() . ' ';
         }
         $php .= '$' . $parameter->getName();
         if ($parameter->isDefaultValueAvailable()) {
@@ -40,20 +59,29 @@ foreach ($functions as $function) {
 
 $php .= N;
 
+
+
+// Classes
 foreach ($classes as $class) {
     $refl = new ReflectionClass($class);
+
+    // Class definition
     $php .= 'class ' . $refl->getName();
     if ($parent = $refl->getParentClass()) {
         $php .= ' extends ' . $parent->getName();
     }
     $php .= N . '{' . N;
+
+    // Properties
     foreach ($refl->getProperties() as $property) {
         $php .= T . '$' . $property->getName() . ';' . N;
     }
+
+    // Methods
     foreach ($refl->getMethods() as $method) {
         if ($method->isPublic()) {
             if ($method->getDocComment()) {
-                $php .= T . $method->getDocComment() . N;                
+                $php .= T . $method->getDocComment() . N;
             }
             $php .= T . 'public function ';
             if ($method->returnsReference()) {
@@ -64,11 +92,11 @@ foreach ($classes as $class) {
                 if ($i >= 1) {
                     $php .= ', ';
                 }
-                if ($parameter->isArray()) {
+                if ($parameter->getType() === 'array') {
                     $php .= 'array ';
                 }
-                if ($typehint = $parameter->getClass()) {
-                    $php .= $typehint->getName() . ' ';
+                if (($type = $parameter->getType()) === 'class') {
+                    $php .= $type->getName() . ' ';
                 }
                 $php .= '$' . $parameter->getName();
                 if ($parameter->isDefaultValueAvailable()) {
@@ -82,4 +110,3 @@ foreach ($classes as $class) {
 }
 
 echo $php . N;
-
